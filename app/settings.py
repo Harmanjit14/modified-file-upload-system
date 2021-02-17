2e3fd4fe2b9d5fa61f482a5b51f888d20d89be39
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-17 15:10:06 +0530

    changes 7

--- app/settings.py
+++ app/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
